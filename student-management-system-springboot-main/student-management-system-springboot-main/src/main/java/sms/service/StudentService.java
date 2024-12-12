b0584933b4ae745c2e573cce40baab287cf17b32
sb2041 <bb136781@gmail.com>
2024-12-12 18:14:52 +0530

    Add files via upload

---
+++ student-management-system-springboot-main/student-management-system-springboot-main/src/main/java/sms/service/StudentService.java
@@ -0,0 +1,17 @@
+package sms.service;
+
+import java.util.List;
+
+import sms.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
